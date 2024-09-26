cc4848de5f53b4610d03951417141ab96e47af8a
Harshit singh <149008610+singhharshitt@users.noreply.github.com>
2024-09-26 23:52:19 +0530

    Update TuplesInClass.cpp

--- TuplesInClass.cpp
+++ TuplesInClass.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
+#include <tuple>  // For using std::tuple
 using namespace std;
-class Person{
+
+class Person {
     string Fname;
     string Lname;
     int age;
-    public:
-    string FL;
-    Person(string f, string l, int a){
-        Fname= f;
-        Lnmae=l;
-        age= a;
+public:
+    string FL; // Full name
+    // Constructor
+    Person(string f, string l, int a) {
+        Fname = f;
+        Lname = l;
+        age = a;
     }
-    void getFullName(){
-        FL=Fname+" "+Lname;
-        cout<<FL<<endl;
+
+    // Method to get the full name
+    void getFullName() {
+        FL = Fname + " " + Lname;
+        cout << "Full Name: " << FL << endl;
     }
-    void getDetails(){
-        cout<<"First Name: "<<Fname<<endl;
-        cout<<"Last Name: "<<Lname<<endl;
-        
+
+    // Method to return details as a tuple
+    tuple<string, string, int> getDetails() {
+        return make_tuple(Fname, Lname, age);
     }
+};
+
+int main() {
+    // Create a Person object
+    Person person("Harshit", "Singh", 20);
+
+    // Call getFullName method
+    person.getFullName();
+
+    // Use structured bindings to unpack the tuple returned by getDetails()
+    auto [firstName, lastName, age] = person.getDetails();
+
+    // Print the details
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Age: " << age << endl;
+
+    return 0;
+}
